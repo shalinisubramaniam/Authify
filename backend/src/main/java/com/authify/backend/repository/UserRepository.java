db61ffcbee5e9e3faf3fc6a3b9fbacd042a28be5
shalinisubramaniam <shalinisubramaniam234@gmail.com>
2025-07-13 14:02:56 +0530

    implemented UserRepository

---
+++ backend/src/main/java/com/authify/backend/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.authify.backend.repository;
+
+import com.authify.backend.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByEmail(String email);
+}
